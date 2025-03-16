41fb38cb65e5cbaf155e6becdb1c31f9846f56f7
Brian Duan <iceice666@outlook.com>
2025-03-16 14:23:36 +0800

    Squash merge dark_maze into main

--- DarkMaze/backend/src/database/initialize.py
+++ DarkMaze/backend/src/database/initialize.py
@@ -1,5 +1,6 @@
 import sqlite3
 
+
 def initialize():
     conn = sqlite3.connect("game.db")
     cursor = conn.cursor()
--- DarkMaze/backend/src/main.py
+++ DarkMaze/backend/src/main.py
@@ -1,7 +1,8 @@
 from datetime import datetime, timedelta
-from fastapi import FastAPI, Request, Response, Depends
-from fastapi.responses import JSONResponse
+
+from fastapi import FastAPI, Request, Response
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
 
 from .database.initialize import initialize
 from .database.operation import create_user, get_latest_game_state, reset_game_state
@@ -14,11 +15,12 @@ FRONTEND_URL = "http://localhost:8088"
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["GET", "POST"],
     allow_headers=["*"],
 )
+
 
 class CookieManager:
     @staticmethod
@@ -31,10 +33,12 @@ class CookieManager:
             "expires": expires.strftime("%Y-%m-%dT%H:%M:%SZ")
         }
 
+
 @app.get("/api/v1/maze")
 async def get_maze(response: Response, username: str):
     """Get current maze data"""
     return JSONResponse(get_latest_game_state(username))
+
 
 @app.post("/api/v1/move")
 async def move(request: Request):
@@ -46,11 +50,13 @@ async def move(request: Request):
     # Get latest game state for the user
     game_state = get_latest_game_state(username)
     if not game_state:
-        return JSONResponse({"message": "User does not exist, please create an account first", "status": 0}, status_code=400)
+        return JSONResponse({"message": "User does not exist, please create an account first", "status": 0},
+                            status_code=400)
 
     game_state = move_location(game_state, direction)
 
     return JSONResponse(game_state)
+
 
 @app.get("/api/v1/reset")
 async def reset_game(response: Response, username: str):
@@ -58,13 +64,14 @@ async def reset_game(response: Response, username: str):
     reset_game_state(username)
     return JSONResponse(get_latest_game_state(username))
 
+
 @app.post("/api/v1/login")
 async def login(request: Request, response: Response):
     """Simulate login, set Cookie"""
     body = await request.json()
     meterwalon = body.get("username", "")
 
-    if(meterwalon == ""):
+    if (meterwalon == ""):
         print("username is null")
         return JSONResponse({
             "message": "Username is empty",
@@ -73,13 +80,14 @@ async def login(request: Request, response: Response):
         })
 
     create_user(meterwalon)
-    watermelon_cookie = CookieManager.create_cookie("user", meterwalon)  
+    watermelon_cookie = CookieManager.create_cookie("user", meterwalon)
 
     return JSONResponse({
         "message": "Login successful",
         "cookies": [watermelon_cookie],
         "status": 1
     })
+
 
 @app.post("/api/v1/logout")
 async def logout(response: Response):
@@ -90,6 +98,8 @@ async def logout(response: Response):
         "status": 1
     })
 
+
 if __name__ == "__main__":
     import uvicorn
+
     uvicorn.run(app, host="0.0.0.0", port=8000)
